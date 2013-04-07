e48445a534ec4f423b5e8d5a1e35626ac88bc150
Rhema Linder <rhemalinder@gmail.com>
2013-04-07 17:38:03 -0500

    More wiimote and networking stuff.

--- network/python/forward_wiimote_data.py
+++ network/python/forward_wiimote_data.py
@@ -1,4 +1,3 @@
-
 s = "/wii/1/accel/"
 
 import socket
@@ -7,16 +6,22 @@ import math
 UDP_IP = "127.0.0.1"
 UDP_PORT = 9000
 
+
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+
+UDP_IP_TO = "127.0.0.1"
+UDP_PORT_TO = 9010
+socksend = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 numero = 0
 numero_poll = 1
 somesome = 0
 
 #3e:fe:e0:cf:3f:09:4b:35:3e:d3:0a:4d
-
+print "here"
 while True:
     numero+=1
     data, addr = sock.recvfrom(80)# buffer size is 1024 bytes
@@ -29,12 +34,14 @@ while True:
     ###print ':'.join(x.encode('hex') for x in xb),':'.join(x.encode('hex') for x in yb),':'.join(x.encode('hex') for x in zb)
     #hex = data.split('/wii/1/accel/pry/3')[1].split(',')[1][-4:]
     #hstring = ':'.join(x.encode('hex') for x in hex)
-    
+    "there"
     if numero%numero_poll == 0:
+        print "inhere"
         x,y,z = struct.unpack('!f', xb),struct.unpack('!f', yb),struct.unpack('!f', zb)
         x,y,z = x[0],y[0],z[0]
         #print x,y,z
         tot = math.sqrt( (x-.5)*(x-.5) + (y-.5)*(y-.5) + (z-.5)*(z-.5) )
         print tot
+        socksend.sendto(str(tot), (UDP_IP_TO, UDP_PORT_TO))
         #print struct.unpack('!i', yb)
         #print struct.unpack('!I', yb)
